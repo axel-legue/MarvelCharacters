2a3d1d381e6da4536b1745e7d56dde0f5e9aade5
axel-legue <axel.legue31@gmail.com>
2021-04-25 22:43:19 +0200

    chore: setup dependency injection for http client

--- app/src/main/java/com/axell/marvelcharacters/MarvelCharacterApplication.kt
+++ app/src/main/java/com/axell/marvelcharacters/MarvelCharacterApplication.kt
@@ -4,6 +4,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MarvelCharacterApplication: Application() {
-
-}
+class MarvelCharacterApplication : Application()
--- app/src/main/java/com/axell/marvelcharacters/di/NetworkModule.kt
+++ app/src/main/java/com/axell/marvelcharacters/di/NetworkModule.kt
@@ -1,21 +1,48 @@
 package com.axell.marvelcharacters.di
 
-import android.content.Context
+import com.axell.marvelcharacters.BuildConfig
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 class NetworkModule {
 
     @Provides
-    fun provideRetrofit(@ApplicationContext appContext: Context): Retrofit {
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor).build()
+    }
+
+    @Provides
+    fun provideMoshiConverterFactory(): MoshiConverterFactory {
+        return MoshiConverterFactory.create()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        httpClient: OkHttpClient,
+        moshiConverterFactory: MoshiConverterFactory
+    ): Retrofit {
         return Retrofit.Builder()
-            .baseUrl("")
+            .baseUrl(
+                BuildConfig.SERVER_URL
+            )
+            .client(httpClient)
+            .addConverterFactory(moshiConverterFactory)
             .build()
     }
 }
