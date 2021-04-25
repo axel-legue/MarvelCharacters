7c683329672d3c53a53eec6f661a1eb43878a57d
axel-legue <axel.legue31@gmail.com>
2021-04-26 01:34:34 +0200

    chore: enable timber logs

--- app/src/main/java/com/axell/marvelcharacters/MarvelCharacterApplication.kt
+++ app/src/main/java/com/axell/marvelcharacters/MarvelCharacterApplication.kt
@@ -2,6 +2,20 @@ package com.axell.marvelcharacters
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+import timber.log.Timber.DebugTree
+
+
+
 
 @HiltAndroidApp
-class MarvelCharacterApplication : Application()
+class MarvelCharacterApplication : Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        }
+    }
+}
