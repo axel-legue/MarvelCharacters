2b9da75f966c1ddd15741c01a34e645b42d2e3e1
axel-legue <axel.legue31@gmail.com>
2022-01-12 21:59:33 +0100

    wip: add dependencies

--- app/src/main/java/com/axell/marvelcharacters/features/characters/ui/CharactersFragment.kt
+++ app/src/main/java/com/axell/marvelcharacters/features/characters/ui/CharactersFragment.kt
@@ -5,10 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import coil.compose.rememberImagePainter
 import com.axell.marvelcharacters.R
 import com.axell.marvelcharacters.core.platform.BaseFragment
 import com.axell.marvelcharacters.databinding.FragmentCharactersBinding
@@ -67,6 +80,49 @@ class CharactersFragment : BaseFragment(R.layout.fragment_characters) {
             characters.clear()
             characters.addAll(it)
             adapter.notifyDataSetChanged()
+        }
+    }
+
+    @Composable
+    fun CharacterList(characters: List<CharacterView>){
+        LazyColumn{
+            items(characters){ character ->
+                Character(name = character.name, picture =character.thumbnail )
+            }
+        }
+    }
+
+    @Composable
+    fun Character(name: String, picture: String) {
+        Row {
+            Image(
+                painter = rememberImagePainter(picture),
+                contentDescription = null,
+                modifier = Modifier.size(128.dp)
+            )
+            Text(
+                text = name,
+                style = MaterialTheme.typography.body2,
+                modifier = Modifier.padding(horizontal = 0.dp, vertical = 5.dp)
+            )
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun CharacterPreview() {
+        Row {
+            Image(
+                painter = rememberImagePainter("https://picsum.photos/200"),
+                contentDescription = null,
+                modifier = Modifier.size(128.dp)
+            )
+            Text(
+                text = "Spiderman",
+                style = MaterialTheme.typography.body2,
+                modifier = Modifier.padding(horizontal = 0.dp, vertical = 5.dp)
+            )
+
         }
     }
 }
